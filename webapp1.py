1e6f1581070dc0fc8c09c4e7eef7ac12b260e4d0
greez <greez76@gmail.com>
2019-10-18 12:49:45 +0300

    add html render

--- webapp1.py
+++ webapp1.py
@@ -2,11 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("home.html")
 
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
